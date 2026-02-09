d53dc620f36cc1cbce9d1895ea7a8414f88c612a
lllzt <lllzt@pm.me>
2026-02-09 13:26:14 +0800

    init

---
+++ lib/earss.ex
@@ -0,0 +1,18 @@
+defmodule Earss do
+  @moduledoc """
+  Documentation for `Earss`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Earss.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/earss_test.exs
@@ -0,0 +1,8 @@
+defmodule EarssTest do
+  use ExUnit.Case
+  doctest Earss
+
+  test "greets the world" do
+    assert Earss.hello() == :world
+  end
+end
